b31fb257ae7078016007afb96dd80a4d57e5fb77
Pin <pinipinipin@yahoo.com>
2022-05-23 20:57:44 +0100

    pasta criada para be

---
+++ ranu-be/controllers/medico.js
@@ -0,0 +1,24 @@
+import pool from '../database/keys';
+
+const medico = {};
+
+// NASCIMENTOS
+medico.registerNascimento = async (req, res)=>{
+    const {numero, nseq, processo, nome, dta_nascimento, hora, gestacao, peso, sexo, puerperio, local_nascimento, apgar1, apgar5} = req.body;
+    console.log(numero, nseq, processo, nome, dta_nascimento, hora, gestacao, peso, sexo, puerperio, local_nascimento, apgar1, apgar5)
+    try {
+        await pool.query('INSERT INTO rn_nascimentos (numero, nseq, processo, nome, dta_nascimento, hora, gestacao, peso, sexo, puerperio, local_nascimento, apgar1, apgar5) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)', [numero,nseq,processo,nome,dta_nascimento,hora,gestacao,peso,sexo,puerperio,local_nascimento,apgar1,apgar5]);
+        //const nascimento = await (await pool.query('SELECT * FROM rn_nascimentos ORDER BY numero DESC LIMIT 1')).rows[0]; 
+        res.status(200).json({
+            message: 'Nascimento foi registado com sucesso!',
+            nascimento: {nome, hora, peso}
+        })
+    } catch {
+        res.status(500).json({
+            message: 'An error has occured',
+        })
+    }
+};
+
+
+module.exports = medico;
--- routes/medico.routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import medico from '../controllers/medico';
-
-const router = express.Router();
-
-router.post('/nascimento', medico.registerNascimento);
-
-module.exports = router;
