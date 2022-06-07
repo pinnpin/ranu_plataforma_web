1b0a2a9d88aa4f388100d01779cbf23fc4f5c591
velorett-i <58183062+velorett-i@users.noreply.github.com>
2022-06-07 15:18:44 +0100

    Add files via upload

--- ranu-be/controllers/tecnico.js
+++ ranu-be/controllers/tecnico.js
@@ -3,13 +3,14 @@ import pool from "../database/keys";
 const tecnico = {};
 
 // NASCIMENTOS
-tecnico.registerNascimento = async (req, res)=>{
-    const {numero, nseq, processo, nome, dta_nascimento, hora, gestacao, peso, sexo, puerperio, local_nasc, apgar1, apgar5} = req.body;
+tecnico.registerNascimento = async (req, res) => {
+    const {nseq, processo, nome, dta_nascimento, hora, gestacao, peso, sexo, puerperio, local_nasc, apgar1, apgar5} = req.body;
     try {
-      await pool.query('INSERT INTO rn_nascimentos (numero,nseq,processo,nome,dta_nascimento,hora,gestacao,peso,sexo,puerperio,local_nasc,apgar1,apgar5) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)', [numero,nseq,processo,nome,dta_nascimento,hora,gestacao,peso,sexo,puerperio,local_nasc,apgar1,apgar5]);
-        res.status(200).json({
-            message: 'Nascimento foi registado com sucesso!',
-            nascimento: {nome, hora, peso}
+      await pool.query('INSERT INTO rn_nascimentos (nseq,processo,nome,dta_nascimento,hora,gestacao,peso,sexo,puerperio,local_nasc,apgar1,apgar5) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)', [numero,nseq,processo,nome,dta_nascimento,hora,gestacao,peso,sexo,puerperio,local_nasc,apgar1,apgar5]);
+      const nascimento = await (await pool.query('SELECT * FROM rn_nascimentos ORDER BY numero DESC LIMIT 1')).rows[0];
+      res.status(200).json({
+          message: 'Nascimento foi registado com sucesso!',
+          nascimento
         })
     } catch (error) {
       res.status(500).json({
@@ -17,7 +18,7 @@ tecnico.registerNascimento = async (req, res)=>{
           error
       })
   }
-}
+};
 /*
 tecnico.deleteNascimento = async (req, res) => {
   const id = req.params.id_t;
